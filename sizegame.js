deca92292eb808298f8fb654a991de94eae8086c
Michael Edgington <57604493+medgington00@users.noreply.github.com>
2023-01-01 01:00:50 -0500

    Add files via upload
    Tweaked menu
    Adjusted button timings
    Misc bugfixes

--- sizegame.js
+++ sizegame.js
@@ -13,7 +13,9 @@ const howToPlayTextContainer = document.getElementById("how-to-play-text-contain
 const settingsTextContainer = document.getElementById("settings-text-container");
 const aboutTextContainer = document.getElementById("about-text-container");
 
+const resetButtonPopup = document.getElementById("reset-button-popup");
 const highScoreText = document.getElementById("high-score-text");
+if(!localStorage.getItem("highscore")) {localStorage.setItem("highscore","0")}
 highScoreText.textContent = "HIGH SCORE: " + localStorage.getItem("highscore");
 
 const gameplayContainer = document.getElementById("gameplay-container");
@@ -31,7 +33,7 @@ const lifeOne = document.getElementById("life-1");
 const lifeTwo = document.getElementById("life-2");
 const lifeThree = document.getElementById("life-3");
 
-const timerBar = document.getElementById("timer-bar");
+const lowerButton = document.getElementById("lower-button");
 
 const popup = document.getElementById("popup");
 const popupBackground = document.getElementById("popup-background");
@@ -40,13 +42,10 @@ const popupExplainMessageOne = document.getElementById("popup-explain-message-1"
 const popupExplainMessageTwo = document.getElementById("popup-explain-message-2");
 const popupExplainMessageThree = document.getElementById("popup-explain-message-3");
 const popupButton = document.getElementById("popup-button");
+const popupHomeButton = document.getElementById("popup-home-button");
 
-const settingsPopup = document.getElementById("settings-popup");
 const flagOnyModeCheckbox = document.getElementById("flag-only-switch");
 const isAudioEnabledCheckbox = document.getElementById("audio-switch");
-
-//flagOnyModeCheckbox.checked = false;
-//isAudioEnabledCheckbox.checked = false;
 
 //Setup variables
 let correct = 0;
@@ -68,11 +67,6 @@ let range = 50;
 let flagOnlyMode = 0;
 let isAudioEnabled = 0;
 
-const roundTime = 5;
-let timer = "";
-
-
-
 if(isAudioEnabledCheckbox.checked == true) {
 	isAudioEnabled = 1;
 }
@@ -81,6 +75,9 @@ if(flagOnyModeCheckbox.checked == true) {
 }
 
 setFontSize();
+
+let lightMode = 0;
+setColorMode();
 
 const correctNoise = new Audio("sounds/correct.mp3");
 const incorrectNoise = new Audio("sounds/incorrect.mp3");
@@ -95,6 +92,8 @@ menuContainer.style.opacity = "1";
 
 function newGame() {
 	closePopup();
+	lowerButton.style.display = "none";
+	popupHomeButton.style.display = "none";
 	popupButton.onclick = nextRound;
 	life = 3;
 	lifeThree.classList.remove("life-container-empty");
@@ -106,14 +105,14 @@ function newGame() {
 }
 
 function nextRound() {
+	setLevel();
+	lowerButton.style.display = "none";
 	buttonOneSpan.style.transition = "0s";
 	buttonOneSpan.style.transform = "scale(0)";
 	countryOrText.style.transition = "0s";
 	countryOrText.style.transform = "scale(0)";
 	buttonTwoSpan.style.transition = "0s";
 	buttonTwoSpan.style.transform = "scale(0)";
-	
-	
 	
 	streakBox.textContent = streak;
 	let tempArr = getRandomCountries(range);
@@ -138,15 +137,10 @@ function nextRound() {
 	setTimeout( function(){buttonTwoSpan.style.transition = "1s";}, 1500);
 	setTimeout( function(){buttonTwoSpan.style.transform = "scale(1)";}, 1500);
 	setTimeout( function(){if(isAudioEnabled == 1) { whooshNoise.play(); };}, 1500);
-	//setTimeout( function(){startTimer();}, 2000);
 	
-}
-
-function startTimer() {
-	//timerBar.style.width = "66.6%";
-	//setTimeout( function(){timerBar.style.width = "33.3%";}, (1000*roundTime / 3));
-	//setTimeout( function(){timerBar.style.width = "0%";}, (1000*roundTime / 3) * 2);
-	//timer = setTimeout(function(){ checkAnswer(0)}, (1000*roundTime)+325);
+	setTimeout( function(){countryOneButton.onclick = function(){checkAnswer(1)}}, 2200);
+	setTimeout( function(){countryTwoButton.onclick = function(){checkAnswer(2)}}, 2200);
+	
 }
 
 //Returns an array with two random countries within maxRankDifference places of each other on the ranking scale
@@ -193,8 +187,10 @@ function setTextContent() {
 }
 
 function checkAnswer(buttonClicked) {
-	clearTimeout(timer);
 	let isCorrect = buttonClicked == correct;
+	
+	countryOneButton.onclick = "";
+	countryTwoButton.onclick = "";
 	
 	let main = "";
 	let mainColor = "";
@@ -205,14 +201,11 @@ function checkAnswer(buttonClicked) {
 	
 	if(isCorrect) {
 		popupButton.onclick = function(){ nextRound() };
-		timerBar.style.width = "100%";
-		timerBar.style.backgroundColor = "green%";
 		if(isAudioEnabled == 1) { correctNoise.play(); }
 		main = "CORRECT!";
 		mainColor = "green";
 		button = "NEXT ROUND";
 		streak++;
-		setLevel();
 	} else if(life>1) {
 		popupButton.onclick = function(){ nextRound() };
 		if(isAudioEnabled == 1) { incorrectNoise.play(); }
@@ -220,9 +213,9 @@ function checkAnswer(buttonClicked) {
 		mainColor = "red";
 		button = "NEXT ROUND";
 		removeLife();
-		setLevel();
 	} else {
 		popupButton.onclick = function(){ newGame() };
+		popupHomeButton.style.display = "inline";
 		if(parseInt(streak) > localStorage.getItem("highscore")) {
 			localStorage.setItem("highscore",streak);
 		}
@@ -270,6 +263,10 @@ function getSizeDifference(larger, smaller) {
 	temp *= 1000;
 	temp = Math.round(temp);
 	temp /= 1000;
+	
+	if(temp=="Infinity") {
+		temp = larger*2;
+	}
 	
 	return temp;
 }
@@ -398,16 +395,6 @@ function closePopup() {
 	popupBackground.style.visibility = "hidden";
 }
 
-function openSettingsPopup() {
-	settingsPopup.classList.add("popup-enabled");
-	popupBackground.style.visibility = "visible";
-}
-
-function closeSettingsPopup() {
-	settingsPopup.classList.remove("popup-enabled");
-	popupBackground.style.visibility = "hidden";
-}
-
 function toggleFlagOnlyMode() {
 	
 	if(flagOnlyMode == 0) {
@@ -435,33 +422,22 @@ function setFontSize() {
 	}
 }
 
-function home() {
-	
-	gameplayElementsContainer.classList.add("hidden");
-	//homeButton.classList.add("hidden");
-	
-	homeElementsContainer.classList.remove("hidden");
-	howToPlayButton.classList.remove("hidden");
-	aboutButton.classList.remove("hidden");
-	settingsButton.classList.remove("hidden");
-	
-	clearTimeout(timer);
-	setTimeout(function(){clearTimeout(timer)},4000);
-}
-
-function play() {
-	homeElementsContainer.classList.add("hidden");
-	howToPlayButton.classList.add("hidden");
-	aboutButton.classList.add("hidden");
-	settingsButton.classList.add("hidden");
-	
-	gameplayElementsContainer.classList.remove("hidden");
-	homeButton.classList.remove("hidden");
-	
-	newGame();
+function showLowerButton() {
+	if(life == 0) {
+		lowerButton.textContent = "TRY AGAIN";
+		lowerButton.onclick = newGame;
+		lowerButton.style.display = "flex";
+	} else {
+		lowerButton.textContent = "NEXT ROUND";
+		lowerButton.onclick = nextRound;
+		lowerButton.style.display = "flex";
+	}
 }
 
 function clickHome() {
+	closePopup();
+	popupHomeButton.style.display = "none";
+	
 	highScoreText.textContent = "HIGH SCORE: " + localStorage.getItem("highscore");
 	playButton.onclick = clickPlay;
 	menuContainer.style.opacity= "0";
@@ -487,8 +463,25 @@ function clickBegin() {
 }
 
 function clickReset() {
+	resetButtonPopup.style.transform = "translate(-50%,-50%)";
+	
+	popupBackground.onclick = closeResetPopup;
+	popupBackground.style.visibility = "visible";
+	
+}
+
+function confirmReset() {
 	localStorage.setItem("highscore","0");
 	highScoreText.textContent = "HIGH SCORE: 0";
+	
+	closeResetPopup();
+}
+
+function closeResetPopup() {
+	resetButtonPopup.style.transform = "translate(-50%,-1000%)";
+	
+	popupBackground.style.visibility = "hidden";
+	popupBackground.onclick = closePopup;
 }
 
 function clickPlay() {
@@ -496,7 +489,7 @@ function clickPlay() {
 	settingsTextContainer.style.display = "none";
 	aboutTextContainer.style.display = "none";
 	
-	playTextContainer.style.display = "inline";
+	playTextContainer.style.display = "flex";
 	
 	howToPlayButton.classList.add("menu-button");
 	howToPlayButton.classList.remove("menu-button-pressed");
@@ -514,7 +507,7 @@ function clickHowToPlay() {
 	settingsTextContainer.style.display = "none";
 	aboutTextContainer.style.display = "none";
 	
-	howToPlayTextContainer.style.display = "inline";
+	howToPlayTextContainer.style.display = "flex";
 	
 	playButton.classList.add("menu-button");
 	playButton.classList.remove("menu-button-pressed");
@@ -532,7 +525,7 @@ function clickSettings() {
 	howToPlayTextContainer.style.display = "none";
 	aboutTextContainer.style.display = "none";
 	
-	settingsTextContainer.style.display = "inline";
+	settingsTextContainer.style.display = "flex";
 	
 	playButton.classList.add("menu-button");
 	playButton.classList.remove("menu-button-pressed");
@@ -550,7 +543,7 @@ function clickAbout() {
 	howToPlayTextContainer.style.display = "none";
 	settingsTextContainer.style.display = "none";
 	
-	aboutTextContainer.style.display = "inline";
+	aboutTextContainer.style.display = "flex";
 	
 	playButton.classList.add("menu-button");
 	playButton.classList.remove("menu-button-pressed");
@@ -563,8 +556,28 @@ function clickAbout() {
 	aboutButton.classList.remove("menu-button");
 }
 
+function setColorMode() {
+	if(lightMode == 1) {
+		document.documentElement.style.setProperty('--base', '#BBCBD2');
+		document.documentElement.style.setProperty('--dark', '#89A4B0');
+		document.documentElement.style.setProperty('--darker', '#5D8191');
+		document.documentElement.style.setProperty('--light', '#FCFDFD');
+		document.documentElement.style.setProperty('--lighter', '#E4EAED');
+		
+		document.documentElement.style.setProperty('--text', '#333333');
+	} else {
+		document.documentElement.style.setProperty('--base', '#2E2B3F');
+		document.documentElement.style.setProperty('--dark', '#211D3B');
+		document.documentElement.style.setProperty('--darker', '#14102E');
+		document.documentElement.style.setProperty('--light', '#4C4866');
+		document.documentElement.style.setProperty('--lighter', '#686582');
+		
+		document.documentElement.style.setProperty('--text', '#FFFFFF');
+	}
+}
+
 function setCountryListString() {
-	let countryListRaw = "1,Russia,17098246,🇷🇺,2,Canada,9984670,🇨🇦,3,China,9596961,🇨🇳,4,United States,9525067,🇺🇸,5,Brazil,8515767,🇧🇷,6,Australia,7692024,🇦🇺,7,India,3287263,🇮🇳,8,Argentina,2780400,🇦🇷,9,Kazakhstan,2724900,🇰🇿,10,Algeria,2381741,🇩🇿,11,DR Congo,2344858,🇨🇩,12,Saudi Arabia,2149690,🇸🇦,13,Mexico,1964375,🇲🇽,14,Indonesia,1904569,🇮🇩,15,Sudan,1861484,🇸🇩,16,Libya,1759540,🇱🇾,17,Iran,1648195,🇮🇷,18,Mongolia,1564110,🇲🇳,19,Peru,1285216,🇵🇪,20,Chad,1284000,🇹🇩,21,Niger,1267000,🇳🇪,22,Angola,1246700,🇦🇴,23,Mali,1240192,🇲🇱,24,South Africa,1221037,🇿🇦,25,Colombia,1141748,🇨🇴,26,Ethiopia,1104300,🇪🇹,27,Bolivia,1098581,🇧🇴,28,Mauritania,1030700,🇲🇷,29,Egypt,1002450,🇪🇬,30,Tanzania,945087,🇹🇿,31,Nigeria,923768,🇳🇬,32,Venezuela,916445,🇻🇪,33,Pakistan,881913,🇵🇰,34,Namibia,825615,🇳🇦,35,Mozambique,801590,🇲🇿,36,Turkey,783562,🇹🇷,37,Chile,756102,🇨🇱,38,Zambia,752612,🇿🇲,39,Myanmar,676578,🇲🇲,40,Afghanistan,652867,🇦🇫,41,South Sudan,644329,🇸🇸,42,France,640679,🇫🇷,43,Somalia,637657,🇸🇴,44,Central African Republic,622984,🇨🇫,45,Ukraine,603550,🇺🇦,46,Madagascar,587041,🇲🇬,47,Botswana,581730,🇧🇼,48,Kenya,580367,🇰🇪,49,Yemen,555000,🇾🇪,50,Thailand,513120,🇹🇭,51,Spain,505992,🇪🇸,52,Turkmenistan,488100,🇹🇲,53,Cameroon,475442,🇨🇲,54,Papua New Guinea,462840,🇵🇬,55,Sweden,450295,🇸🇪,56,Uzbekistan,447400,🇺🇿,57,Morocco,446550,🇲🇦,58,Iraq,438317,🇮🇶,59,Paraguay,406752,🇵🇾,60,Zimbabwe,390757,🇿🇼,61,Norway,385207,🇳🇴,62,Japan,377976,🇯🇵,63,Germany,357114,🇩🇪,64,Congo,342000,🇨🇬,65,Finland,338425,🇫🇮,66,Vietnam,331212,🇻🇳,67,Malaysia,330803,🇲🇾,68,Ivory Coast,322463,🇨🇮,69,Poland,312696,🇵🇱,70,Oman,309500,🇴🇲,71,Italy,301339,🇮🇹,72,Philippines,300000,🇵🇭,73,Ecuador,276841,🇪🇨,74,Burkina Faso,274222,🇧🇫,75,New Zealand,270467,🇳🇿,76,Gabon,267668,🇬🇦,77,Guinea,245857,🇬🇳,78,United Kingdom,242495,🇬🇧,79,Uganda,241550,🇺🇬,80,Ghana,238533,🇬🇭,81,Romania,238397,🇷🇴,82,Laos,236800,🇱🇦,83,Guyana,214969,🇬🇾,84,Belarus,207600,🇧🇾,85,Kyrgyzstan,199951,🇰🇬,86,Senegal,196722,🇸🇳,87,Syria,185180,🇸🇾,88,Cambodia,181035,🇰🇭,89,Uruguay,176215,🇺🇾,90,Suriname,163820,🇸🇷,91,Tunisia,163610,🇹🇳,92,Bangladesh,148460,🇧🇩,93,Nepal,147516,🇳🇵,94,Tajikistan,143100,🇹🇯,95,Greece,131957,🇬🇷,96,Nicaragua,130373,🇳🇮,97,Eritrea,125000,🇪🇷,98,North Korea,120540,🇰🇵,99,Malawi,118484,🇲🇼,100,Benin,114763,🇧🇯,101,Honduras,112492,🇭🇳,102,Liberia,111369,🇱🇷,103,Bulgaria,111002,🇧🇬,104,Cuba,109884,🇨🇺,105,Guatemala,108889,🇬🇹,106,Iceland,103000,🇮🇸,107,South Korea,100210,🇰🇷,108,Hungary,93028,🇭🇺,109,Portugal,92226,🇵🇹,110,Jordan,89342,🇯🇴,111,Serbia,88361,🇷🇸,112,Azerbaijan,86600,🇦🇿,113,Austria,83871,🇦🇹,114,United Arab Emirates,83600,🇦🇪,115,Czech Republic,78871,🇨🇿,116,Panama,75417,🇵🇦,117,Sierra Leone,71740,🇸🇱,118,Ireland,70273,🇮🇪,119,Georgia,69700,🇬🇪,120,Sri Lanka,65610,🇱🇰,121,Lithuania,65300,🇱🇹,122,Latvia,64559,🇱🇻,123,Togo,56785,🇹🇬,124,Croatia,56594,🇭🇷,125,Bosnia and Herzegovina,51209,🇧🇦,126,Costa Rica,51100,🇨🇷,127,Slovakia,49037,🇸🇰,128,Dominican Republic,48671,🇩🇴,129,Estonia,45227,🇪🇪,130,Denmark,43094,🇩🇰,131,Netherlands,41850,🇳🇱,132,Switzerland,41284,🇨🇭,133,Bhutan,38394,🇧🇹,134,Guinea-Bissau,36125,🇬🇼,135,Moldova,33846,🇲🇩,136,Belgium,30528,🇧🇪,137,Lesotho,30355,🇱🇸,138,Armenia,29743,🇦🇲,139,Solomon Islands,28896,🇸🇧,140,Albania,28748,🇦🇱,141,Equatorial Guinea,28051,🇬🇶,142,Burundi,27834,🇧🇮,143,Haiti,27750,🇭🇹,144,Rwanda,26338,🇷🇼,145,North Macedonia,25713,🇲🇰,146,Djibouti,23200,🇩🇯,147,Belize,22966,🇧🇿,148,El Salvador,21041,🇸🇻,149,Israel,20770,🇮🇱,150,Slovenia,20273,🇸🇮,151,Fiji,18272,🇫🇯,152,Kuwait,17818,🇰🇼,153,Eswatini,17364,🇸🇿,154,Timor-Leste,14919,🇹🇱,155,The Bahamas,13943,🇧🇸,156,Montenegro,13812,🇲🇪,157,Vanuatu,12189,🇻🇺,158,Qatar,11586,🇶🇦,159,The Gambia,11295,🇬🇲,160,Jamaica,10991,🇯🇲,161,Lebanon,10452,🇱🇧,162,Cyprus,9251,🇨🇾,163,Palestinian Territory,6020,🇵🇸,164,Brunei,5765,🇧🇳,165,Trinidad and Tobago,5130,🇹🇹,166,Cape Verde,4033,🇨🇻,167,Samoa,2842,🇼🇸,168,Luxembourg,2586,🇱🇺,169,Mauritius,2040,🇲🇺,170,Comoros,1862,🇰🇲,171,São Tomé and Príncipe,964,🇸🇹,172,Kiribati,811,🇰🇮,173,Bahrain,786,🇧🇭,174,Dominica,751,🇩🇲,175,Tonga,747,🇹🇴,176,Singapore,728,🇸🇬,177,Micronesia,702,🇫🇲,178,Saint Lucia,616,🇱🇨,179,Andorra,468,🇦🇩,180,Palau,459,🇵🇼,181,Seychelles,452,🇸🇨,182,Antigua and Barbuda,442,🇦🇬,183,Barbados,430,🇧🇧,184,Saint Vincent and the Grenadines,389,🇻🇨,185,Grenada,344,🇬🇩,186,Malta,316,🇲🇹,187,Maldives,300,🇲🇻,188,Saint Kitts and Nevis,261,🇰🇳,189,Marshall Islands,181,🇲🇭,190,Liechtenstein,160,🇱🇮,191,San Marino,61,🇸🇲,192,Tuvalu,26,🇹🇻,193,Nauru,21,🇳🇷,194,Monaco,2.02,🇲🇨,195,Vatican City,0.51,🇻🇦"
+	let countryListRaw = "1,Russia,17098246,🇷🇺,2,Canada,9984670,🇨🇦,3,China,9596961,🇨🇳,4,United States,9525067,🇺🇸,5,Brazil,8515767,🇧🇷,6,Australia,7692024,🇦🇺,7,India,3287263,🇮🇳,8,Argentina,2780400,🇦🇷,9,Kazakhstan,2724900,🇰🇿,10,Algeria,2381741,🇩🇿,11,DR Congo,2344858,🇨🇩,12,Saudi Arabia,2149690,🇸🇦,13,Mexico,1964375,🇲🇽,14,Indonesia,1904569,🇮🇩,15,Sudan,1861484,🇸🇩,16,Libya,1759540,🇱🇾,17,Iran,1648195,🇮🇷,18,Mongolia,1564110,🇲🇳,19,Peru,1285216,🇵🇪,20,Chad,1284000,🇹🇩,21,Niger,1267000,🇳🇪,22,Angola,1246700,🇦🇴,23,Mali,1240192,🇲🇱,24,South Africa,1221037,🇿🇦,25,Colombia,1141748,🇨🇴,26,Ethiopia,1104300,🇪🇹,27,Bolivia,1098581,🇧🇴,28,Mauritania,1030700,🇲🇷,29,Egypt,1002450,🇪🇬,30,Tanzania,945087,🇹🇿,31,Nigeria,923768,🇳🇬,32,Venezuela,916445,🇻🇪,33,Pakistan,881913,🇵🇰,34,Namibia,825615,🇳🇦,35,Mozambique,801590,🇲🇿,36,Turkey,783562,🇹🇷,37,Chile,756102,🇨🇱,38,Zambia,752612,🇿🇲,39,Myanmar,676578,🇲🇲,40,Afghanistan,652867,🇦🇫,41,South Sudan,644329,🇸🇸,42,France,640679,🇫🇷,43,Somalia,637657,🇸🇴,44,Central African Republic,622984,🇨🇫,45,Ukraine,603550,🇺🇦,46,Madagascar,587041,🇲🇬,47,Botswana,581730,🇧🇼,48,Kenya,580367,🇰🇪,49,Yemen,555000,🇾🇪,50,Thailand,513120,🇹🇭,51,Spain,505992,🇪🇸,52,Turkmenistan,488100,🇹🇲,53,Cameroon,475442,🇨🇲,54,Papua New Guinea,462840,🇵🇬,55,Sweden,450295,🇸🇪,56,Uzbekistan,447400,🇺🇿,57,Morocco,446550,🇲🇦,58,Iraq,438317,🇮🇶,59,Paraguay,406752,🇵🇾,60,Zimbabwe,390757,🇿🇼,61,Norway,385207,🇳🇴,62,Japan,377976,🇯🇵,63,Germany,357114,🇩🇪,64,Congo,342000,🇨🇬,65,Finland,338425,🇫🇮,66,Vietnam,331212,🇻🇳,67,Malaysia,330803,🇲🇾,68,Ivory Coast,322463,🇨🇮,69,Poland,312696,🇵🇱,70,Oman,309500,🇴🇲,71,Italy,301339,🇮🇹,72,Philippines,300000,🇵🇭,73,Ecuador,276841,🇪🇨,74,Burkina Faso,274222,🇧🇫,75,New Zealand,270467,🇳🇿,76,Gabon,267668,🇬🇦,77,Guinea,245857,🇬🇳,78,United Kingdom,242495,🇬🇧,79,Uganda,241550,🇺🇬,80,Ghana,238533,🇬🇭,81,Romania,238397,🇷🇴,82,Laos,236800,🇱🇦,83,Guyana,214969,🇬🇾,84,Belarus,207600,🇧🇾,85,Kyrgyzstan,199951,🇰🇬,86,Senegal,196722,🇸🇳,87,Syria,185180,🇸🇾,88,Cambodia,181035,🇰🇭,89,Uruguay,176215,🇺🇾,90,Suriname,163820,🇸🇷,91,Tunisia,163610,🇹🇳,92,Bangladesh,148460,🇧🇩,93,Nepal,147516,🇳🇵,94,Tajikistan,143100,🇹🇯,95,Greece,131957,🇬🇷,96,Nicaragua,130373,🇳🇮,97,Eritrea,125000,🇪🇷,98,North Korea,120540,🇰🇵,99,Malawi,118484,🇲🇼,100,Benin,114763,🇧🇯,101,Honduras,112492,🇭🇳,102,Liberia,111369,🇱🇷,103,Bulgaria,111002,🇧🇬,104,Cuba,109884,🇨🇺,105,Guatemala,108889,🇬🇹,106,Iceland,103000,🇮🇸,107,South Korea,100210,🇰🇷,108,Hungary,93028,🇭🇺,109,Portugal,92226,🇵🇹,110,Jordan,89342,🇯🇴,111,Serbia,88361,🇷🇸,112,Azerbaijan,86600,🇦🇿,113,Austria,83871,🇦🇹,114,United Arab Emirates,83600,🇦🇪,115,Czech Republic,78871,🇨🇿,116,Panama,75417,🇵🇦,117,Sierra Leone,71740,🇸🇱,118,Ireland,70273,🇮🇪,119,Georgia,69700,🇬🇪,120,Sri Lanka,65610,🇱🇰,121,Lithuania,65300,🇱🇹,122,Latvia,64559,🇱🇻,123,Togo,56785,🇹🇬,124,Croatia,56594,🇭🇷,125,Bosnia and Herz.,51209,🇧🇦,126,Costa Rica,51100,🇨🇷,127,Slovakia,49037,🇸🇰,128,Dominican Republic,48671,🇩🇴,129,Estonia,45227,🇪🇪,130,Denmark,43094,🇩🇰,131,Netherlands,41850,🇳🇱,132,Switzerland,41284,🇨🇭,133,Bhutan,38394,🇧🇹,134,Guinea-Bissau,36125,🇬🇼,135,Moldova,33846,🇲🇩,136,Belgium,30528,🇧🇪,137,Lesotho,30355,🇱🇸,138,Armenia,29743,🇦🇲,139,Solomon Islands,28896,🇸🇧,140,Albania,28748,🇦🇱,141,Equatorial Guinea,28051,🇬🇶,142,Burundi,27834,🇧🇮,143,Haiti,27750,🇭🇹,144,Rwanda,26338,🇷🇼,145,North Macedonia,25713,🇲🇰,146,Djibouti,23200,🇩🇯,147,Belize,22966,🇧🇿,148,El Salvador,21041,🇸🇻,149,Israel,20770,🇮🇱,150,Slovenia,20273,🇸🇮,151,Fiji,18272,🇫🇯,152,Kuwait,17818,🇰🇼,153,Eswatini,17364,🇸🇿,154,Timor-Leste,14919,🇹🇱,155,The Bahamas,13943,🇧🇸,156,Montenegro,13812,🇲🇪,157,Vanuatu,12189,🇻🇺,158,Qatar,11586,🇶🇦,159,The Gambia,11295,🇬🇲,160,Jamaica,10991,🇯🇲,161,Lebanon,10452,🇱🇧,162,Cyprus,9251,🇨🇾,163,Palestinian Territory,6020,🇵🇸,164,Brunei,5765,🇧🇳,165,Trinidad and Tobago,5130,🇹🇹,166,Cape Verde,4033,🇨🇻,167,Samoa,2842,🇼🇸,168,Luxembourg,2586,🇱🇺,169,Mauritius,2040,🇲🇺,170,Comoros,1862,🇰🇲,171,São Tomé and Príncipe,964,🇸🇹,172,Kiribati,811,🇰🇮,173,Bahrain,786,🇧🇭,174,Dominica,751,🇩🇲,175,Tonga,747,🇹🇴,176,Singapore,728,🇸🇬,177,Micronesia,702,🇫🇲,178,Saint Lucia,616,🇱🇨,179,Andorra,468,🇦🇩,180,Palau,459,🇵🇼,181,Seychelles,452,🇸🇨,182,Antigua and Barbuda,442,🇦🇬,183,Barbados,430,🇧🇧,184,Saint Vincent and the Grenadines,389,🇻🇨,185,Grenada,344,🇬🇩,186,Malta,316,🇲🇹,187,Maldives,300,🇲🇻,188,Saint Kitts and Nevis,261,🇰🇳,189,Marshall Islands,181,🇲🇭,190,Liechtenstein,160,🇱🇮,191,San Marino,61,🇸🇲,192,Tuvalu,26,🇹🇻,193,Nauru,21,🇳🇷,194,Monaco,2.02,🇲🇨,195,Vatican City,0.49,🇻🇦"
 
 	countryListArr = countryListRaw.split(",");
 	
